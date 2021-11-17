568db3664dd90bb75418599e1bf4953c7d49337e
Carlos Dip <cadudip@hotmail.com>
2021-11-17 10:50:15 -0300

    open external connections

--- orm.py
+++ orm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 app = Flask('main')
@@ -17,4 +18,4 @@ def index():
     out = f"<main><ul>{''.join([f'<li>{i[0]} has U${i[1]:.02f} in their account</li>' for i in data])}</ul></main>"
     return out
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
